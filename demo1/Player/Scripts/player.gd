950a02375ebd89fdb466a79901af738bd32db1ae
TIANHAO QU <1696090109@qq.com>
2026-06-24 21:22:07 +0800

    移动

--- demo1/Player/Scripts/player.gd
+++ demo1/Player/Scripts/player.gd
@@ -1,5 +1,6 @@
-extends Node
+class_name Player extends CharacterBody2D
 
+var move_speed : float = 100.0;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -7,5 +8,13 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(_delta: float) -> void:
+	var direction := Vector2.ZERO
+	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	velocity = direction.normalized() * move_speed
+	
+
+
+func _physics_process(_delta: float) -> void:
+	move_and_slide()
