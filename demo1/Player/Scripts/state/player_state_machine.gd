77238a3798ed5ec2f9c12bb8e61f6d5488af5ecf
TIANHAO QU <1696090109@qq.com>
2026-07-01 18:17:27 +0800

    增加 guide 输入处理封装

--- demo1/Player/Scripts/state/player_state_machine.gd
+++ demo1/Player/Scripts/state/player_state_machine.gd
@@ -18,12 +18,31 @@ func _ready() -> void:
 	
 #初始化状态机
 func Initialize(player: Player) -> void:
+
+	#设置初始状态为待机状态
+	player_state_machine.set_initial_state(idle)
 	
+	#设置更新模式为 _physics_process(delta)
+	player_state_machine.update_mode = LimboHSM.PHYSICS
+	#设置更新模式为 _process(delta)
+	#player_state_machine.update_mode = LimboHSM.IDLE 
+
 	#初始化时注册态机事件
 	player_state_machine.add_transition(idle,run, &"idle_to_run")
 	player_state_machine.add_transition(run,idle, &"run_to_idle")
-	
-	#初始化状态机控制的节点
+
+	#初始化状态机控制什么节点
 	player_state_machine.initialize(player)
 	#设置状态机为启动
 	player_state_machine.set_active(true)
+
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
