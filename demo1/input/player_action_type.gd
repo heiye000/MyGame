701bca862c1b327d768c88366d41c6258de57c8f
TIANHAO QU <1696090109@qq.com>
2026-07-02 21:22:04 +0800

    键盘攻击

--- demo1/input/player_action_type.gd
+++ demo1/input/player_action_type.gd
@@ -3,11 +3,14 @@ extends RefCounted
 
 ## 玩家行动枚举。新增行动时只需在此添加枚举值与对应资源路径。
 enum Type {
-	MOVE,
+	MOVE, 	#移动
+	ATTACK_LEFT, #攻击-左
+
 }
 
 const _ACTIONS: Dictionary = {
 	Type.MOVE: preload("res://input/actions/move.tres"),
+	Type.ATTACK_LEFT: preload("res://input/actions/attack_left.tres"),
 }
 
 
